06621fa68867731183cf3fd3c7365a19c1860fd4
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-19 17:45:04 -0400

    Successfully delete the first item

--- linked_list/single.c
+++ linked_list/single.c
@@ -5,17 +5,25 @@
 // this is just a file to test my singly linked list
 void main()
 {
+    // make a list to test
     list *mylist = listinit();
+
+    // add some data do it
     char *strings[] = {"monday", "tuesday", "wednesday", "thursday",
          "friday", "saturday", "sunday"};
-
     int i;
-
     for (i = 0; i < 7; i++) {
         insertstart(mylist, nodegen(strings[i]));
     }
 
+    // check
     printlist(mylist);
+    printf("\n");
+
+    // delete the first item
+    deletebeginning(mylist);
+    printlist(mylist);
+
 }
 
 
