e7d985a0e967771f618e240980d6da3a59c50b14
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-19 18:01:28 -0400

    Search the list for a string

--- linked_list/single.c
+++ linked_list/single.c
@@ -15,15 +15,20 @@ void main()
     for (i = 0; i < 7; i++) {
         insertstart(mylist, nodegen(strings[i]));
     }
-
-    // check
     printlist(mylist);
     printf("\n");
 
     // delete the first item
     deletebeginning(mylist);
     printlist(mylist);
+    printf("\n");
 
+    // find 'thursday', add 'pants' afterwards
+    node *thurs = listsearch(mylist, "thursday");
+    insert(thurs, nodegen("pants"));
+    printlist(mylist);
+
+    
 }
 
 
