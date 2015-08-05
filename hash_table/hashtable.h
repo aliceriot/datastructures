0e39a0eb0766a0183cbacb94a5f0f4b717aa1e06
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-05 15:13:37 -0400

    copying over linked list stuff

--- hash_table/hashtable.h
+++ hash_table/hashtable.h
@@ -5,6 +5,19 @@
 
 unsigned char *hash(unsigned char *key);
 
+// linked list structs
+typedef struct node {
+    struct node *previous;
+    struct node *next;
+    char *data;
+} node;
+
+typedef struct list {
+    node *head;
+    node *tail;
+} list;
+
+
 unsigned char *hash(unsigned char *key)
 { // get the hash of a key
     size_t len = sizeof(key);
@@ -15,5 +28,11 @@ unsigned char *hash(unsigned char *key)
 
 doublelist **hashtab(int size)
 { // allocates hashtable array, returns pointer to array
-    doublelist *hashtab[size]
+    doublelist *hashtab[size];
+
+    int i;
+    for (i = 0; i < size; i++) {
+        memset(hashtab[i], *void, sizeof(*doublelist));
+    }
+    return doublelist;
 }
