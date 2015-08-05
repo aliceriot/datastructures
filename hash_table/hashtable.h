f8eec9ecee1ad93db7630cccd2ce5d1854e9543a
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-05 16:08:30 -0400

    Removing nodes! Adding them!

--- hash_table/hashtable.h
+++ hash_table/hashtable.h
@@ -3,7 +3,17 @@
 #include <stdlib.h>
 #include <openssl/sha.h>
 
-// linked list structs
+/* My hashtable implementation. This code is free to use/re-use/whatever */
+/* if you find it helpful */
+/* :D */
+
+/* This is a hashtable using SHA1 - we use a linked list implementation */
+/* I wrote to provide for separate chaining. Every bin in the table */ 
+/* is initialized pointing to an empty linked list, if we hash a key */ 
+/* that points to that node then we add things to that list (key/value pairs) */
+/* then later we can look in that list to find everything hashed to that key! */
+
+// structs
 typedef struct node {
     struct node *previous;
     struct node *next;
@@ -16,7 +26,6 @@ typedef struct list {
     node *tail;
 } list;
 
-// hashtable struct
 typedef struct hashtable {
     int size;
     list *table[];
@@ -24,7 +33,7 @@ typedef struct hashtable {
 
 // function declarations
 unsigned char *hash(unsigned char *key);
-list **hashinit(int size);
+hashtable *hashinit(int size);
 void destroyhash(hashtable *oldtable);
 list *listinit();
 void destroylist(list *oldlist);
@@ -94,5 +103,19 @@ void destroylist(list *oldlist)
     }
     free(sentinel);
     free(oldlist);
+}
+
+void listinsert(list *insertlist, node *toinsert)
+{ // inserts a new item at the beginning
+    toinsert->next = insertlist->head;
+    toinsert->prepend = insertlist->tail;
+    insertlist->head = toinsert;
+}
+
+void listremove(list *insertlist, node *toremove)
+{ // remove a given node (use listsearch to find it)
+    toremove->next->previous = toremove->previous;
+    toremove->previous->next = toremove->next;
+    free(toremove);
 }
 
